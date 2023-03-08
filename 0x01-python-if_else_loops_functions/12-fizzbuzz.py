ac8854289fbeed64fcf6f2c96a9f9def963a9fcc
Allan Kiarie <allan.kiarie91@gmail.com>
2023-03-08 00:48:24 -0800

    Correction:reduces characters in line 6 and add 'end='

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,15 +2,15 @@
 def fizzbuzz():
     '''Prints numbers from 1 to 100 separated by a space.
     If the number is a multiple of 3 print Fizz instead of the number.
-    If the number is a multiple of 5 print Buzz instead of the number.
-    If the number is a multiiple of both 3 and 5 print FizzBuzz instead of the number.
+    If the number is a multiple of 5 print Buzz instead of the nummber
+    Print FizzBuzz instead of the number for multiples of 3 and 5.
     '''
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ")
+            print("FizzBuzz ", end="")
         elif number % 3 == 0:
-            print("Fizz ")
+            print("Fizz ", end="")
         elif number % 5 == 0:
-            print("Buzz ")
+            print("Buzz ", end="")
         else:
-            print("{} ".format(number))
+            print("{} ".format(number), end="")
