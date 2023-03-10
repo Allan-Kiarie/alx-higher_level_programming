b7284c954e996258f6dbefb85f6ba8e05795f737
Allan Kiarie <allan.kiarie91@gmail.com>
2023-03-10 11:09:46 -0800

    Reduce the length of the comment to meet pycodestyle

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,6 +2,8 @@
 
 from variable_load_5 import a
 if __name__ == "__main__":
-    '''imports the variable a from the file variable_load_5.py and prints its value'''
+    '''imports the variable a from variable_load_5.
+    Then prints its value.
+    '''
 
     print("{}".format(a))
