ec208e583e419e9960d713495cc1413b0eb4c916
Allan Kiarie <allan.kiarie91@gmail.com>
2023-03-15 11:15:00 -0700

    Add solution for task 6

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    '''prints a dictionary by ordered keys'''
+
+    mykeys = list(a_dictionary.keys())
+    mykeys.sort()
+    sorted_dict = {i: a_dictionary[i] for i in mykeys}
