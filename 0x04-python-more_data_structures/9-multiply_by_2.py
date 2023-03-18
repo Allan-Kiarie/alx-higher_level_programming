c4bead82ee314ba8a748c0526f7d12b7212094d2
Allan Kiarie <allan.kiarie91@gmail.com>
2023-03-18 00:35:33 -0700

    corrrect newDict[key]

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -6,6 +6,6 @@ def multiply_by_2(a_dictionary):
     newDict = {}
 
     for key, value in a_dictionary.items():
-        newDict = value * 2
+        newDict[key] = value * 2
 
     return (newDict)
