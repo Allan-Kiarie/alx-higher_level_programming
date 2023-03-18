5ea3cee1f26a35066045f9e7b52e8ac972bc95c5
Allan Kiarie <allan.kiarie91@gmail.com>
2023-03-18 07:09:40 -0700

    Add solution to task 15

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    '''deletes keys with a specific value in a dictionary'''
+
+    keys_del = [k for (k, v) in a_dictionary.items() if v == value]
+
+    for key in keys_del:
+        del a_dictionary[key]
