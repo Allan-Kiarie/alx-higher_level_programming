db239781593f7addefe3a62c56b1cb307c7e9537
Allan Kiarie <allan.kiarie91@gmail.com>
2023-04-06 12:12:02 -0700

    Access and update private attribute of class Square

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A representation of object square"""
+
+    def __init__(self, size=0):
+        """Initialization of a new Square.
+
+        Args:
+            size(int): size of the new Square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Access the attribute size in class Square"""
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns area of current square"""
+        return (self.__size ** 2)
