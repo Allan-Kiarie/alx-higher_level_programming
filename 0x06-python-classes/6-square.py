b183dd3679b2f8a031e06b03d241eac8c821f086
Allan Kiarie <allan.kiarie91@gmail.com>
2023-04-07 04:19:27 -0700

    Add whitespace on line 8 after ',' in tuple and add identation to lines 39 to 42

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """A representation of object square"""
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         """Initialization of a new Square.
 
         Args:
@@ -36,10 +36,10 @@ class Square:
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
-        len(value) != 2 or
-        not isinstance(value[0], int) or
-        not isinstance(value[1], int) or
-        value[0] < 0 or value[1] < 0):
+           len(value) != 2 or
+           not isinstance(value[0], int) or
+           not isinstance(value[1], int) or
+           value[0] < 0 or value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -52,7 +52,6 @@ class Square:
         if self.__size == 0:
             print()
             return
-
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
