8945cd6320a73c438ced5311b4be9cb26ae50936
Allan Kiarie <allan.kiarie91@gmail.com>
2023-06-15 00:10:51 -0700

    Print the methods and attributes of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-lookup.py
+"""Defines a function that returns methods and
+attributes of an object"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
+    return dir(obj)
