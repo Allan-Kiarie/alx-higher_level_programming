97b92b6fd0b3bbdc5c6d9fc88ac3b71a852e4657
Allan Kiarie <allan.kiarie91@gmail.com>
2023-06-16 12:57:40 -0700

    add class Rectangle that inherits from BaseGeometry

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -21,6 +21,6 @@ class BaseGeometry:
             ValueError: value less than or equal to 0
         """
         if not isinstance(value, int):
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
