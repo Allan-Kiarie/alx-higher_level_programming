6388cb38cfe23845d43c106a53c2a882d8dea808
Allan Kiarie <allan.kiarie91@gmail.com>
2023-06-20 08:33:27 -0700

    Write the JSON string representation of list_objs to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,6 +22,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """Returns the JSON string representation of list_dictionaries.
 
@@ -31,3 +32,20 @@ class Base:
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file.
+
+        Args:
+            list_objs(list): a list of instances who inherits of Base.
+        """
+        if list_objs is None:
+            list_objs = []
+
+        file_name = cls.__name__ + ".json"
+
+        with open(file_name, 'w') as jfile:
+            list_dicts = [obj.to_dictionary() for obj in list_objs]
+            json_str = cls.to_json_string(list_dicts)
+            jfile.write(json_str)
