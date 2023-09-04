79b1dc00d3fa9b51f36009e074e39bf098626152
C. Allan <118625728+Allan-Kiarie@users.noreply.github.com>
2023-09-04 08:32:07 +0300

    Delete 0x0A-python-inheritance directory
    To redo the programs saved in the directory

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# 0-lookup.py
-"""Defines a function that returns methods and
-attributes of an object"""
-
-
-def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# 5-base_geometry.py
-"""Defines an empty class."""
-
-
-class BaseGeometry:
-    """An empty class"""
-    pass
